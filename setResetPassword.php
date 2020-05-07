d33db4931e6143eed32356bb1f7ad40e2ab73de0
Diarmuid1812 <szustak.m.6@gmail.com>
2020-05-07 16:46:06 +0200

    Łączenie

---
+++ setResetPassword.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Script to set password first time a user logs in
+ * or reset it after login.
+ */
+
+// Initialize the session
+session_start();
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: usrLogin.php");
+    exit;
+}
+
+// Include config file
+require_once "config_db.php";
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Pole nie może być puste.";
+    }
+    elseif(strlen(trim($_POST["new_password"])) < 5)
+    {
+        $new_password_err = "Hasło musi mieć co najmniej 5 znaków.";
+    }
+    else
+    {
+        $new_password = trim($_POST["new_password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"])))
+    {
+        $confirm_password_err = "Wprowadź ponownie hasło.";
+    }
+    else
+    {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($new_password_err) && ($new_password != $confirm_password))
+        {
+            $confirm_password_err = "Hasła nie są identyczne!";
+        }
+    }
+
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET password = :password WHERE id = :id";
+
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            $stmt->bindParam(":id", $param_id, PDO::PARAM_INT);
+
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: usrLogin.php");
+                exit();
+            } else{
+                echo "Coś poszło nie tak. Spróbuj ponownie później";
+            }
+
+            // Close statement
+            unset($stmt);
+        }
+    }
+
+    // Close connection
+    unset($pdo);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <h2>Zresetuj hasło</h2>
+    <p>Wprowadź i potwierdź nowe hasło</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+            <label>Nowe hasło
+            <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
+            <span class="help-block"><?php echo $new_password_err; ?></span>
+            </label>
+        </div>
+        <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <label>Powtórz hasło
+            <input type="password" name="confirm_password" class="form-control">
+            <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </label>
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Zmień">
+            <a class="btn btn-link" href="interfejsGlowny.phtml">Powrót</a>
+        </div>
+    </form>
+</div>
+</body>
+</html>
